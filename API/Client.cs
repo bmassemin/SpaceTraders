3ec391a3c8afda282eed662d8a5b16b74ab8d28b
Baptiste <baptiste.massemin@gmail.com>
2023-05-09 01:41:16 +0200

    Initial commit

---
+++ API/Client.cs
@@ -0,0 +1,13 @@
+﻿using Refit;
+using SpaceTraders.API;
+
+namespace SpaceTraders;
+
+internal interface IClient
+{
+    [Post("/v2/register")]
+    Task<DataResponse> RegisterNewAgentAsync([Body] RegisterNewAgentRequest request);
+
+    [Get("/v2/my/agent")]
+    Task<DataGenerics<Agent>> AgentDetailsAsync();
+}
---
+++ RetryHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+
+namespace SpaceTraders;
+
+internal class RetryHandler : DelegatingHandler
+{
+    public RetryHandler()
+    {
+        InnerHandler = new HttpClientHandler();
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        var response = await base.SendAsync(request, cancellationToken);
+        if (response.IsSuccessStatusCode)
+            return response;
+
+        while (response.StatusCode == HttpStatusCode.TooManyRequests)
+        {
+            await Task.Delay(1000, cancellationToken);
+            response = await base.SendAsync(request, cancellationToken);
+        }
+
+        return response;
+    }
+}
